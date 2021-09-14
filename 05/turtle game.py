49c7963e2a8efc0b4a6b0cd9c5405d969d735e44
chodott <89974193+chodott@users.noreply.github.com>
2021-09-14 19:10:27 +0900

    DRILL #5 완성 (최종)
    https://www.youtube.com/watch?v=OXvjq6F9TOM

--- 05/turtle game.py
+++ 05/turtle game.py
@@ -1,20 +1,27 @@
 import turtle as t
 
 def go_up():
+    t.stamp()
     t.setheading(90)
     t.forward(50)
 
 def go_down():
+    t.stamp()
     t.setheading(270)
     t.forward(50)
 
 def go_left():
+    t.stamp()
     t.setheading(180)
     t.forward(50)
 
 def go_right():
+    t.stamp()
     t.setheading(0)
     t.forward(50)
+
+def go_escape():
+    t.reset()
 
 t.shape("turtle")
 
@@ -22,6 +29,7 @@ t.onkeypress(go_up,"w")
 t.onkeypress(go_down,"s")
 t.onkeypress(go_left,"a")
 t.onkeypress(go_right,"d")
+t.onkeypress(go_escape,"Escape")
 
 t.listen()
                              
